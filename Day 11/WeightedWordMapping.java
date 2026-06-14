92103b48e9a3a7121d16b5eb7869f141fbaffba1
Atharva-hub-123 <atharvappathak22@gmail.com>
2026-06-14 09:27:55 +0530

    Day 11 DSA Solutions

---
+++ Day 11/WeightedWordMapping.java
@@ -0,0 +1,21 @@
+// LeetCode 3838 - Weighted Word Mapping
+// https://leetcode.com/problems/weighted-word-mapping/description/
+
+class Solution {
+    public String mapWordWeights(String[] words, int[] weights) {
+        StringBuilder ans = new StringBuilder();
+
+        for (String word : words) {
+            long sum = 0;
+
+            for (char ch : word.toCharArray()) {
+                sum += weights[ch - 'a'];
+            }
+
+            int mod = (int)(sum % 26);
+            ans.append((char)('z' - mod));
+        }
+        return ans.toString();
+    }
+}
+
