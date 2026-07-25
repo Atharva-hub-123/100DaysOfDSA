f3ab6eb3c1ab508b1de030cc20350f128f806b64
Atharva-hub-123 <atharvappathak22@gmail.com>
2026-07-25 21:44:27 +0530

    Day 53 Solutions

---
+++ Day 53/Main.java
@@ -0,0 +1,23 @@
+// Codeforces 1337A. Ichihime and Triangle
+// https://codeforces.com/problemset/problem/1337/A
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            int d = sc.nextInt();
+
+            System.out.println(b + " " + c + " " + c);
+        }
+
+        sc.close();
+    }
+}
---
+++ Day 53/MaximumProductOfTwoDigits.java
@@ -0,0 +1,22 @@
+// Leetcode 3536 - Maximum Product of Two Digits
+// https://leetcode.com/problems/maximum-product-of-two-digits/description/
+
+class Solution {
+    public int maxProduct(int n) {
+        int first = 0;
+        int second = 0;
+        while(n > 0){
+            int digit = n % 10;
+            if(digit > first){
+                second = first;
+                first = digit;
+            }
+            else if(digit > second){
+                second = digit;
+            }
+            n /= 10;
+        }
+
+        return first * second; 
+    }
+}
