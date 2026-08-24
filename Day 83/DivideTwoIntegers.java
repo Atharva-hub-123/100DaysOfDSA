476c1b1b224fecf9cef56018b690a8a6ef2dcfad
Atharva-hub-123 <atharvappathak22@gmail.com>
2026-08-24 23:55:07 +0530

    Day 83 Solutions

---
+++ Day 83/DivideTwoIntegers.java
@@ -0,0 +1,40 @@
+// LeetCode 29 - Divide Two Integers
+// https://leetcode.com/problems/divide-two-integers/description/
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+         if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+
+        boolean negative = (dividend < 0) ^ (divisor < 0);
+
+        long dvd = Math.abs((long) dividend);
+        long dvs = Math.abs((long) divisor);
+
+        long quotient = 0;
+
+        while (dvd >= dvs) {
+
+            long temp = dvs;
+            long multiple = 1;
+
+            while (dvd >= (temp << 1)) {
+                temp <<= 1;
+                multiple <<= 1;
+            }
+
+            dvd -= temp;
+
+            quotient += multiple;
+        }
+
+        if (negative) {
+            quotient = -quotient;
+        }
+
+        return (int) quotient;
+    }
+}
+  
